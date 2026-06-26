c10e31e00edc3c1ff0e29735c89a0e8dc94a72ea
unknown <gargvinayak18@gmail.com>
2026-06-26 10:57:43 +0530

    Favicon fixed

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -72,7 +72,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     ],
     links: [
       { rel: "stylesheet", href: appCss },
-      { rel: "icon", type: "image/png", href: "/favicon.png" },
+      { rel: "icon", type: "image/png", href: "/favicon.png?v=2" },
     ],
   }),
   shellComponent: RootShell,
